83c0f0273c1fff7d6fc7c33e2a254ccea1a81eb3
alvian <alvian.nabhan@jamkrindo.co.id>
2024-01-17 09:24:54 +0800

    update progress

---
+++ application/views/company-satu/blankPage.php
@@ -0,0 +1,66 @@
+<div class="container">
+                <div class="row" style="padding-top:15px;"> 
+                    <div class="col-md-9">
+                        <div class="blog-post single-post">
+						
+                            <article class="post post-medium" style="padding-bottom: 10px;">
+								<div class="row">
+										<div class="col-md-5">
+											<div class="img-responsive">
+												
+											</div>
+										</div>
+										<div class="col-md-7">
+
+											<div class="post-content">
+
+
+											</div>
+										</div>
+										
+
+									<div class="col-md-7">
+										<div class="post-meta">
+											<span><i class="fas fa-business-time"></i></i></span>
+                                            <p style="font-size: 20px;">Not Found Job Vacancy</p>
+										</div>
+									</div>
+								</div>
+
+							</article>
+
+                        </div>
+						
+						<div class="pagination--wrapper clearfix bdtop--1 bd--color-2 ptop--60 pbottom--30">
+											
+							<ul class="pagination float--right">
+								<li>
+								</li>
+							</ul>
+							
+						</div>
+						
+                    </div>
+
+                    <div class="col-md-3">
+                        <aside class="siderbar">
+							<h4 class="heading-primary"></h4>
+                            <span><i class="fa-solid fa-business-time"></i></i></span>
+
+                            <hr />
+							
+                            <h4 class="heading-primary"></h4>
+                            <ul class="nav nav-list mb-xlg">
+                                    
+                            </ul>
+                            <hr />
+                            
+                       
+                        </aside>
+                    </div>
+                </div>
+            </div>
+       
+
+
+
